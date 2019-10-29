caeb90a4f24279fb54fc722aa93f50456c4c4244
Victor Arteaga <xathovic@gmail.com>
2019-10-29 14:43:36 +0000

    add condition percent to the NULL

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@ int _printf(const char *format, ...)
 	va_list arg;
 	int i, len = 0, bytes = 0;
 
-	if (format == NULL)
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	va_start(arg, format);
 	for (i = 0; format[i] != '\0'; i++)
